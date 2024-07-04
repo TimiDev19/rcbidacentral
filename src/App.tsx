5c7737e97714bf353d3a7a311a438b392fb5955c
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-07-03 19:45:48 -0700

    opening animation

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,37 @@ import About from './pages/About';
 import Newsletter from './pages/Newsletter';
 import Contact from './pages/Contact';
 import Participate from './pages/Participate';
+import { useEffect, useState } from 'react';
 
 function App() {
-
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 4000)
+  })
 
   return (
     <div>
-      <BrowserRouter>
-        <Sidebar />
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/newsletter' element={<Newsletter />} />
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/participate' element={<Participate/>}/>
-        </Routes>
-      </BrowserRouter>
+      {
+        isLoading ? (<div className=' h-[100vh] w-full flex items-center justify-center bg-[#17458f]'>
+          <div className=' w-fit'>
+            <h1 className=' font-bold text-3xl md:text-7xl text-white'>RC BIDA</h1>
+            <div className=' underlineanimation h-[3px] bg-[#f7a81b]'></div>
+          </div>
+        </div>) : (<div>
+          <BrowserRouter>
+            <Sidebar />
+            <Routes>
+              <Route path='/' element={<LandingPage />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/newsletter' element={<Newsletter />} />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='/participate' element={<Participate />} />
+            </Routes>
+          </BrowserRouter>
+        </div>)
+      }
     </div>
   )
 }
